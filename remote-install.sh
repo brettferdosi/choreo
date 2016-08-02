478767776906d13089d5723f049aeb16df664aae
Brett Gutstein <brett@gutste.in>
2016-08-02 02:25:46 +0100

    allow updates with remote install

--- remote-install.sh
+++ remote-install.sh
@@ -3,6 +3,11 @@
 if [ ! -d ~/dotfiles ]; then
   echo "cloning dotfiles repo to ~/dotfiles..."
   git clone https://github.com/brettferdosi/dotfiles.git ~/dotfiles
+else
+  echo "pulling updates to ~/dotfiles..."
+  cd ~/dotfiles
+  git pull
+  cd -
 fi
 
 echo "running dotfiles installer..."

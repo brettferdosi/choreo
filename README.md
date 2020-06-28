272fe4d8b66aaded33f9f06763747c65817f2a89
Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
2020-06-27 23:17:03 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+run `git clone http://dotfiles.gutste.in dotfiles` then `install.sh`
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# install things by cloning the repo then running this script
-
 DIR="`cd "\`dirname "$0"\`" && pwd -P`"
 
 if [ "$#" -eq "0" ]; then
